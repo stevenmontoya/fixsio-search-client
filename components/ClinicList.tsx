eab0ea6e42feb62d05d19d75f5f5fd74f52b354c
Steven Montoya <stevenmontoya@MacBook-Pro-de-Steven.local>
2022-03-30 18:04:40 +0200

    fix: fix export and import interface

--- components/ClinicList.tsx
+++ components/ClinicList.tsx
@@ -2,7 +2,10 @@ import { Clinic, useClinics } from "../lib/useClinics";
 import { LocalCard } from "./LocalCard";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Clinics } from "../pages/clinics/types";
+
+export interface Clinics {
+  clinics: any[];
+}
 
 export const ClinicList: React.FC<Clinics> = ({ clinics }) => {
   const router = useRouter();
--- pages/clinics/types.ts
+++
@@ -1,3 +0,0 @@
-export default interface Clinics {
-  clinics: any[];
-}
