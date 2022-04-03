736f6ba1fef7bc74b04b8a856c9f5de60186b791
Steven Montoya <stevenmontoya@MacBook-Pro-de-Steven.local>
2022-04-03 23:16:46 +0200

    feat: implement recoil store to coords and clinics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import { GlobalStyles } from "../components/GlobalStyles";
+import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }) => (
   <div>
     <GlobalStyles />
-    <Component {...pageProps} />
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   </div>
 );
 
---
+++ recoil/atom/clinicsAtom.tsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const clinicsAtom = atom({
+  key: "clinicsAtom",
+  default: [],
+});
