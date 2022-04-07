cdd8e91407bba16756f2fcfca3ee2510133b1aef
Steven Montoya <stevenmontoya@MacBook-Pro-de-Steven.local>
2022-04-07 21:38:13 +0200

    feat: add modal to show phone to contact on clinic detail page

--- components/ClinicContact.tsx
+++ components/ClinicContact.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import tw from "twin.macro";
 import PhoneIcon from "./icons/phoneIcon";
 import ScheduleIcon from "./icons/scheduleIcon";
+import { Modal } from "./Modal";
+import contants from "../utils/contants.json";
 
-export const ClinicContact = () => {
-  const buttonWrapper = (children) => {
+interface ClinicContact {
+  phone: string;
+}
+
+export const ClinicContact: React.FC<ClinicContact> = ({ phone }) => {
+  const [infoToShow, setInfoToShow] = useState(null);
+
+  const handleCloseModal = () => {
+    setInfoToShow(null);
+  };
+
+  const handleClinicContact = (infoToShow) => {
+    setInfoToShow(infoToShow);
+  };
+
+  const buttonWrapper = (children, infoToShow) => {
     return (
-      <div tw="bg-indigo-100 pl-6 pr-6 pt-2 pb-2 rounded-lg">{children}</div>
+      <div
+        tw="bg-indigo-100 pl-6 pr-6 pt-2 pb-2 rounded-lg"
+        onClick={() => handleClinicContact(infoToShow)}
+      >
+        {children}
+      </div>
     );
   };
+
   return (
     <>
-      <div tw="flex justify-around">
-        {buttonWrapper(<ScheduleIcon />)}
-        {buttonWrapper(<PhoneIcon />)}
+      <div tw="flex justify-around mt-10">
+        {buttonWrapper(<PhoneIcon />, contants.ModalPhoneText + phone)}
       </div>
+      {infoToShow && (
+        <Modal infoToShow={infoToShow} onClose={handleCloseModal} />
+      )}
     </>
   );
 };
